a29db73e7792a5dab210940093bbc3b768f2e3f6
Patrik Hildenborg Mac <phildenborg@gmail.com>
2026-04-19 03:25:34 +0200

    feat: add validation for tarot card count (1-3)

--- API/DTOs/Tarot/TarotInterpretRequest.cs
+++ API/DTOs/Tarot/TarotInterpretRequest.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace SarasBloggAPI.DTOs.Tarot;
 
 public class TarotInterpretRequest
 {
     public string Question { get; set; } = string.Empty;
+    [MinLength(1, ErrorMessage = "At least 1 card is required.")]
+    [MaxLength(3, ErrorMessage = "Maximum 3 cards allowed.")]
     public List<string> Cards { get; set; } = new();
     public string Language { get; set; } = "en";
 }
